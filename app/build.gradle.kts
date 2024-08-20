333a45f48efbad8b915beb8ba273fbccebc4e0aa
Sarthak Roy <sarthakroy2002@gmail.com>
2024-08-20 16:21:30 +0530

    SOS: Misc changes
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,17 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
